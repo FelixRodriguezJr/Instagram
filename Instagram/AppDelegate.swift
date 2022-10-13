1058006bfbd185578dea20959ee337d6c0799ec6
Felix <79116375+FelixRodriguezJr@users.noreply.github.com>
2022-10-13 00:30:07 -0400

    Update fix

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "5Qlga1zwvXdC2pYiqrkJUnW9IsVbfgRnOUqMpMeO" // <- UPDATE
+                $0.clientKey = "2WIIuTfudCF77w987bLE8qupP6imObQBWpAtSPda" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com/"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
